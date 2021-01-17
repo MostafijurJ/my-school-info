6225b8ca88a6da8a577d258094e49d023fdfb6be
mostafijurj <mrkajol550@gmail.com>
2021-01-17 15:55:05 +0600

    task done till assignment-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CardComponent } from './shared/component/card/card.component';
+import { BoxListComponent } from './shared/component/box-list/box-list.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { CardComponent } from './shared/component/card/card.component';
     HomeComponent,
     AboutComponent,
     NotFoundComponent,
-    CardComponent
+    CardComponent,
+    BoxListComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,21 +8,35 @@ import {Card} from "../../shared/interfaces";
 })
 export class HomeComponent implements OnInit {
 
-  public cardList : Card [] =[]
+  public studentTitle:string="Student List";
+  public teacherTitle:string="Teacher List";
+
+  public studentList : Card [] = [];
+  public teacherList : Card [] = [];
 
   constructor() {
-    this.cardList =this.getCardList();
+    this.studentList =this.getStudentList();
+    this.teacherList = this.getTeacherList();
   }
 
   ngOnInit(): void {
   }
 
-  private getCardList(): Card[] {
+  private getStudentList(): Card[] {
 
     let cards: Card[] = [];
-    cards.push({title: 'HTML', counter: 1, frequency: 'week', text: 'you need to work with this'});
-    cards.push({title: 'JS', counter: 2, frequency: 'week', text: 'you need to work with this'});
-    cards.push({title: 'Angular', counter: 3, frequency: 'week', text: 'you need to work with this'});
+    cards.push({title: 'Alice', counter: 1, frequency: 'week', text: 'you need to work with this'});
+    cards.push({title: 'Bob', counter: 2, frequency: 'week', text: 'you need to work with this'});
+    cards.push({title: 'Jack', counter: 3, frequency: 'week', text: 'you need to work with this'});
+    return cards;
+}
+
+private getTeacherList(): Card[] {
+
+    let cards: Card[] = [];
+    cards.push({title: 'Sanowar Hossain', counter: 1, frequency: 'week', text: 'you need to work with this'});
+    cards.push({title: 'MR. RAHIM', counter: 2, frequency: 'week', text: 'you need to work with this'});
+    cards.push({title: 'Dr. Jahid', counter: 3, frequency: 'week', text: 'you need to work with this'});
     return cards;
 }
 
