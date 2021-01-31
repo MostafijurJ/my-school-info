c9f698d26362b67df3ab05a7b3e7ae7564ed2165
mostafijurj <mrkajol550@gmail.com>
2021-01-31 12:03:10 +0600

    fix: issues in existing

--- src/app/pages/district/district.component.ts
+++ src/app/pages/district/district.component.ts
@@ -12,6 +12,13 @@ export class DistrictComponent implements OnInit {
   public counter = 1;
   public districtList: DistrictInfo [] = [];
 
+  //methods for sorting data
+  get sortData() {
+    return this.districtList.sort((a, b) => {
+      return <any>new Date(b.areaSqKm) - <any>new Date(a.areaSqKm);
+    });
+  }
+
   constructor(private  districtService: DistrictService) {
     this.setDistrictList();
   }
@@ -21,9 +28,8 @@ export class DistrictComponent implements OnInit {
 
   private setDistrictList(): void {
     this.districtService.getDistrictList().then(res => {
-      if (res.serviceResult && res.serviceResult.success === true) {
+      if (res.serviceResult && res.serviceResult.success) {
         this.districtList = res.data;
-        console.log(res)
       } else {
         return console.log("Error" + res);
       }
